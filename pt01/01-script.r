9ab51587da75ee741217b3b88045f36af2244be3
enzodelcompare <enzodel1991@gmail.com>
2021-02-10 00:11:01 -0300

    Parte I - Fundamentos

--- pt01/01-script.r
+++ pt01/01-script.r
@@ -484,86 +484,89 @@ lista_string_3 = list(matrix(c("A", "A", "A", "A"), nr = 2), 'B', 'C')
 lista_string_3
 
 # Lista de números inteiros
-lista_inteiros = list(2, 3, 4)
+lista_inteiros = list(5, 10, 15)
 lista_inteiros
 
 # Lista de floats
-lista_numerico = list(1.90, 45.3, 300.5)
-lista_numerico
+lista_float = list(5.95, 10.25, 100.55)
+lista_float
 
 # Lista de números complexos
-lista_complexos = list(5.2+3i, 2.4+8i)
-lista_complexos
+lista_num_complexos = list(5.2+5i, 2.5+10i)
+lista_num_complexos
 
 # Lista de valores lógicos
 lista_logicos = list(TRUE, FALSE, FALSE)
 lista_logicos
 
-# Listas Compostas
-lista_composta1 = list("A", 3, TRUE)
-lista_composta1
+# Listas compostas
+lista_composta = list("Linguagem R", 100, TRUE)
+lista_composta
 
-lista1 <- list(1:10, c("Zico", "Ronaldo", "Garrincha"), rnorm(10))
-lista1
+lista <- list(1:10, c("Feynman", "Maxwell", "Schrödinger"), rnorm(10))
+lista
 
+# Pedindo ajuda
 ?rnorm
 
-# Slicing (Fatiamento) da Lista
-lista1[1]
-lista1[2]
-lista1[[2]][1]
-lista1[[2]][1] = "Monica"
-lista1
+# Fatiamento da lista
+lista[1]
+lista[2]
+lista[[2]][2]
+lista[[2]][1] = "Ampère"
+lista
 
-# Para nomear os elementos - Listas Nomeadas
-names(lista1) <- c("inteiros", "caracteres", "numéricos")
-lista1
+# Nomeando os elementos de uma lista existente
+names(lista) <- c("Inteiros", "Físicos", "Números")
+lista
 
-vec_num <- 1:4
-vec_char <- c("A", "B", "C", "D")
+vetor_numerico <- 1:5
+vetor_caracteres <- c("Aa", "Bb", "Cc", "Dd", "Ee")
 
-lista2 <- list(Numeros = vec_num, Letras = vec_char)
-lista2
+lista <- list(numeros = vetor_numerico, letras = vetor_caracteres)
+lista
 
-# Nomear os elementos diretamente
-lista2 <- list(elemento1 = 3:5, elemento2 = c(7.2,3.5))
-lista2
+# Nomeando diretamente os elementos
+lista_nomeada <- list(elementoA = 1:5, elementoB = c(5.5, 10.5))
+lista_nomeada
 
 # Trabalhando com elementos específicos da lista
-names(lista1) <- c("inteiros", "caracteres", "numéricos")
-lista1
+lista <- list(1:10, c("Feynman", "Maxwell", "Schrödinger"), rnorm(10))
+lista
+names(lista) <- c("Inteiros", "Físicos", "Números")
+lista
 
-lista1$caracteres
-length(lista1$inteiros)
-lista1$inteiros
+lista$Físicos
+length(lista$Inteiros)
+lista$Inteiros
 
-# Verificar o comprimento da lista
-length(lista1)
+# Verificando o comprimento da lista
+length(lista)
 
-# Podemos extrair um elemento específico dentro de cada nível da lista
-lista1$caracteres[2]
+# Retornando um elementos, dentro de uma nível específico da lista
+lista$Físicos[2]
 
 # Mode dos elementos
-mode(lista1$numéricos)
-mode(lista1$caracteres)
+mode(lista$Números)
+mode(lista$Físicos)
 
-# Combinando 2 listas
-lista3 <- c(lista1, lista2)
-lista3
+# Combinando duas listas
+lista_combinada <- c(lista, lista_composta)
+lista_combinada
 
-# Transformando um vetor em lista
-v = c(1:3)
-v
-l  = as.list(v)
-l
+# Transformando vetor em lista
+vetor = c(1:5)
+vetor
+lista  = as.list(vetor)
+lista
 
-# Unindo 2 elementos em uma lista
-mat = matrix(1:4, nrow = 2)
-mat
-vec = c(1:9)
-vec
-lst = list(mat, vec)
-lst
+# Unindo dois elementos em uma lista
+matriz = matrix(1:10, nrow = 2)
+matriz
+vetor = c(1:10)
+vetor
+lista = list(matriz, vetor)
+lista
 
 # ========== # ========== # ========== # ========== # ========== #
 
