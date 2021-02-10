7eae2e6969bb42314fbfe4d232d3cd5ea9f95614
enzodelcompare <enzodel1991@gmail.com>
2021-02-09 23:17:01 -0300

    Parte I - Fundamentos

--- pt01/01-script.r
+++ pt01/01-script.r
@@ -386,34 +386,36 @@ vetor_nomeado["4"]
 setwd("C:\r-language\pt01")
 getwd()
 
-# Quantidade de Linhas
-matrix (c(1,2,3,4,5,6), nr = 2)
-matrix (c(1,2,3,4,5,6), nr = 3)
-matrix (c(1,2,3,4,5,6), nr = 6)
+# Quantidade de linhas
+matrix(c(1,2,3,4,5,6), nrow = 2)
+matrix(c(1,2,3,4,5,6), nr = 3)
+matrix(c(1,2,3,4,5,6), nr = 6)
 
-# Número de Colunas
-matrix (c( 1,2,3,4,5,6), nc = 2)
+# Quantidade de colunas
+matrix(c( 1,2,3,4,5,6), ncol = 2)
 
-# Help
+# Pedindo ajuda
 ?matrix
 
-# Matrizes precisam ter um número de elementos que seja múltiplo do número de linhas
-matrix (c(1,2,3,4,5), nc = 2)
+# Matrizes: quantidade de elementos múltiplos da quantidade de linhas
+matrix(c(1,2,3,4,5), nc = 2)
 
-# Criando matrizes a partir de vetores e preenchendo a partir das linhas
-meus_dados = c(1:10)
-matrix(data = meus_dados, nrow = 5, ncol = 2, byrow = T)
-matrix(data = meus_dados, nrow = 5, ncol = 2)
+# Criação de matrizes com vetores e alterando o preenchimento para linha
+dados = c(1:10)
+matrix(data = dados, nrow = 5, ncol = 2, byrow = T)
 
-# Fatiando a Matriz
-mat <- matrix(c(2,3,4,5), nr = 2)
-mat
-mat[1,2]
-mat[2,2]
-mat[1,3]
-mat[,2]
+# Do mesmo exemplo acima, mas, com comportamento padrão
+matrix(data = dados, nrow = 5, ncol = 2)
 
-# Criando uma matriz diagonal
+# Fatiando matriz
+matriz <- matrix(c(2,3,4,5), nc = 2)
+matriz
+matriz[1,2]
+matriz[2,2]
+matriz[1,3]
+matriz[,2]
+
+# Criando matriz diagonal
 matriz = 1:3
 diag(matriz)
 
