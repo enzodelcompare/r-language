506fffc768729cac087186d9ed2c772b7c7422d1
enzodelcompare <enzodel1991@gmail.com>
2021-02-09 23:04:15 -0300

    Parte I - Fundamentos

--- pt01/01-script.r
+++ pt01/01-script.r
@@ -379,3 +379,361 @@ vetor_nomeado
 names(vetor_nomeado) = c("1", "2", "3", "4", "5", "6", "7", "8", "9")
 vetor_nomeado
 vetor_nomeado["4"]
+
+# ========== # ========== # ========== # ========== # ========== #
+
+# Configurando o diretório de trabalho
+setwd("C:\r-language\pt01")
+getwd()
+
+# Quantidade de Linhas
+matrix (c(1,2,3,4,5,6), nr = 2)
+matrix (c(1,2,3,4,5,6), nr = 3)
+matrix (c(1,2,3,4,5,6), nr = 6)
+
+# Número de Colunas
+matrix (c( 1,2,3,4,5,6), nc = 2)
+
+# Help
+?matrix
+
+# Matrizes precisam ter um número de elementos que seja múltiplo do número de linhas
+matrix (c(1,2,3,4,5), nc = 2)
+
+# Criando matrizes a partir de vetores e preenchendo a partir das linhas
+meus_dados = c(1:10)
+matrix(data = meus_dados, nrow = 5, ncol = 2, byrow = T)
+matrix(data = meus_dados, nrow = 5, ncol = 2)
+
+# Fatiando a Matriz
+mat <- matrix(c(2,3,4,5), nr = 2)
+mat
+mat[1,2]
+mat[2,2]
+mat[1,3]
+mat[,2]
+
+# Criando uma matriz diagonal
+matriz = 1:3
+diag(matriz)
+
+# Extraindo vetor de uma matriz diagonal
+vetor = diag(matriz)
+diag(vetor)
+
+# Transposta da matriz
+W <- matrix (c(2,4,8,12 ), nr = 2, ncol = 2)
+W
+t(W)
+U <- t(W)
+U
+
+# Obtendo uma matriz inversa
+solve(W)
+
+# Multiplicação de Matrizes
+mat1 <- matrix(c(2,3,4,5), nr = 2)
+mat1
+mat2 <- matrix(c(6,7,8,9), nr = 2)
+mat2
+mat1 * mat2
+mat1 / mat2
+mat1 + mat2
+mat1 - mat2
+
+# Multiplicando Matriz com Vetor
+x = c(1:4)
+x
+y <- matrix(c(2,3,4,5), nr = 2)
+x * y
+
+# Nomeando a Matriz
+mat3 <- matrix(c('Terra', 'Marte', 'Saturno', 'Netuno'), nr = 2)
+mat3
+dimnames(mat3) = (list( c("Linha1", "Linha2"), c("Coluna1", "Coluna2")))
+mat3
+
+# Identificando linhas e colunas no momento de criação da Matriz
+matrix (c(1,2,3,4), nr = 2, nc = 2, dimnames = list(c("Linha 1", "Linha 2" ), c( "Coluna 1", " Coluna 2")))
+
+# Combinando Matrizes
+mat4 <- matrix(c(2,3,4,5), nr = 2)
+mat4
+mat5 <- matrix(c(6,7,8,9), nr = 2)
+mat5
+cbind(mat4, mat5)
+rbind(mat4, mat5)
+
+# Desconstruindo a Matriz
+c(mat4)
+
+# ========== # ========== # ========== # ========== # ========== #
+
+# Configurando o diretório de trabalho
+setwd("C:\r-language\pt01")
+getwd()
+
+# Lista de strings
+lista_caracter1 = list('A', 'B', 'C')
+lista_caracter1
+lista_caracter2 = list(c("A", "A"), 'B', 'C')
+lista_caracter2
+lista_caracter3 = list(matrix(c("A", "A", "A", "A"), nr = 2), 'B', 'C')
+lista_caracter3
+
+# Lista de números inteiros
+lista_inteiros = list(2, 3, 4)
+lista_inteiros
+
+# Lista de floats
+lista_numerico = list(1.90, 45.3, 300.5)
+lista_numerico
+
+# Lista de números complexos
+lista_complexos = list(5.2+3i, 2.4+8i)
+lista_complexos
+
+# Lista de valores lógicos
+lista_logicos = list(TRUE, FALSE, FALSE)
+lista_logicos
+
+# Listas Compostas
+lista_composta1 = list("A", 3, TRUE)
+lista_composta1
+
+lista1 <- list(1:10, c("Zico", "Ronaldo", "Garrincha"), rnorm(10))
+lista1
+
+?rnorm
+
+# Slicing (Fatiamento) da Lista
+lista1[1]
+lista1[2]
+lista1[[2]][1]
+lista1[[2]][1] = "Monica"
+lista1
+
+# Para nomear os elementos - Listas Nomeadas
+names(lista1) <- c("inteiros", "caracteres", "numéricos")
+lista1
+
+vec_num <- 1:4
+vec_char <- c("A", "B", "C", "D")
+
+lista2 <- list(Numeros = vec_num, Letras = vec_char)
+lista2
+
+# Nomear os elementos diretamente
+lista2 <- list(elemento1 = 3:5, elemento2 = c(7.2,3.5))
+lista2
+
+# Trabalhando com elementos específicos da lista
+names(lista1) <- c("inteiros", "caracteres", "numéricos")
+lista1
+
+lista1$caracteres
+length(lista1$inteiros)
+lista1$inteiros
+
+# Verificar o comprimento da lista
+length(lista1)
+
+# Podemos extrair um elemento específico dentro de cada nível da lista
+lista1$caracteres[2]
+
+# Mode dos elementos
+mode(lista1$numéricos)
+mode(lista1$caracteres)
+
+# Combinando 2 listas
+lista3 <- c(lista1, lista2)
+lista3
+
+# Transformando um vetor em lista
+v = c(1:3)
+v
+l  = as.list(v)
+l
+
+# Unindo 2 elementos em uma lista
+mat = matrix(1:4, nrow = 2)
+mat
+vec = c(1:9)
+vec
+lst = list(mat, vec)
+lst
+
+# ========== # ========== # ========== # ========== # ========== #
+
+# Configurando o diretório de trabalho
+setwd("C:\r-language\pt01")
+getwd()
+
+# String
+texto <- "Isso é uma string!"
+texto
+
+x = as.character(3.14)
+x
+class(x)
+
+# Concatenando Strings
+nome = "Nelson"; sobrenome = "Mandela"
+paste(nome, sobrenome)
+cat(nome, sobrenome)
+
+# Formatando a saída
+sprintf("A %s é nota %d", "Data Science Academy", 10)
+
+# Extraindo parte da string
+texto <- "Isso é uma string!"
+substr(texto, start=12, stop=17)
+?substr
+
+# Contando o número de caracteres
+nchar(texto)
+
+# Alterando a capitalização
+tolower("Histogramas e Elementos de Dados")
+toupper("Histogramas e Elementos de Dados")
+
+# Usando stringr
+library(stringr)
+
+# Dividindo uma string em caracteres
+?strsplit
+strsplit("Histogramas e Elementos de Dados", NULL)
+
+# Dividindo uma string em caracteres, após o caracter espaço
+strsplit("Histogramas e Elementos de Dados", " ")
+
+# Trabalhando com strings
+string1 <- c("Esta é a primeira parte da minha string e será a primeira parte do meu vetor",
+             "Aqui a minha string continua, mas será transformada no segundo vetor")
+string1
+
+string2 <- c("Precisamos testar outras strings - @???!§$",
+             "Análise de Dados em R")
+string2
+
+# Adicionando 2 strings
+str_c(c(string1, string2), sep = "")
+
+# Podemos contar quantas vezes um caracter aparece no texto
+str_count(string2, "s")
+
+# Localiza a primeira e última posição em que o caracter aparece na string
+str_locate_all(string2, "s")
+
+# Substitui a primeira ocorrência de um caracter
+str_replace(string2, "\\s", "")
+
+# Substitui todas as ocorrências de um caracter
+str_replace_all(string2, "\\s", "")
+
+# Detectando padrões nas strings
+string1 <- "17 jan 2001"
+string2 <- "1 jan 2001"
+padrao <- "jan 20"
+grepl(pattern = padrao, x = string1)
+padrao <- "jan20"
+grepl(pattern = padrao, x = string1)
+
+# ========== # ========== # ========== # ========== # ========== #
+
+# Configurando o diretório de trabalho
+setwd("C:\r-language\pt01")
+getwd()
+
+# Criando um dataframe vazio
+df <- data.frame()
+class(df)
+df
+
+# Criando vetores vazios
+nomes <- character()
+idades <- numeric()
+itens <- numeric()
+codigos <- integer()
+
+df <- data.frame(c(nomes, idades, itens, codigos))
+df
+
+# Criando vetores
+pais = c("Portugal", "Inglaterra", "Irlanda", "Egito", "Brasil")
+nome = c("Bruno", "Tiago", "Amanda", "Bianca", "Marta")
+altura = c(1.88, 1.76, 1.53, 1.69, 1.68)
+codigo = c(5001, 2183, 4702, 7965, 8890)
+
+# Criando um dataframe de diversos vetores
+pesquisa = data.frame(pais, nome, altura, codigo)
+pesquisa
+
+# Adicionando um novo vetor a um dataframe existente
+olhos = c("verde", "azul", "azul", "castanho", "castanho")
+pesq = cbind(pesquisa, olhos)
+pesq
+
+# Informações sobre o dataframe
+str(pesq)
+dim(pesq)
+length(pesq)
+
+# Obtendo um vetor de um dataframe
+pesq$pais
+pesq$nome
+
+# Extraindo um único valor
+pesq[1,1]
+pesq[3,2]
+
+# Número de Linhas e Colunas
+nrow(pesq)
+ncol(pesq)
+
+# Primeiros elementos do dataframe
+head(pesq)
+head(mtcars)
+
+# Últimos elementos do dataframe
+tail(pesq)
+tail(mtcars)
+
+# Data frames built-in do R
+?mtcars
+mtcars
+View(mtcars)
+
+# Filtro para um subset de dados que atendem a um critério
+pesq[altura < 1.60,]
+pesq[altura < 1.60, c('codigo', 'olhos')]
+pesq
+
+# Dataframes Nomeados
+names(pesq) <- c("País", "Nome", "Altura", "Código", "Olhos")
+pesq
+
+colnames(pesq) <- c("Var 1", "Var 2", "Var 3", "Var 4", "Var 5")
+rownames(pesq) <- c("Obs 1", "Obs 2", "Obs 3", "Obs 4", "Obs 5")
+pesq
+
+# Carregando um arquivo csv
+?read.csv
+pacientes <- data.frame(read.csv(file = 'pacientes.csv', header = TRUE, sep = ","))
+
+# Visualizando o dataset
+View(pacientes)
+head(pacientes)
+summary(pacientes)
+
+# Visualizando as variáveis
+pacientes$Diabete
+pacientes$status
+pacientes$Status
+
+# Histograma
+hist(pacientes$Idade)
+
+# Combinando dataframes
+dataset_final <- merge(pesq, pacientes)
+dataset_final
