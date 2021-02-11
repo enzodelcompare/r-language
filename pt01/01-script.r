152d9b0e86c5ee77fe2cdca4000407d4b9b3a8ad
enzodelcompare <enzodel1991@gmail.com>
2021-02-10 21:47:18 -0300

    Parte I - Fundamentos

--- pt01/01-script.r
+++ pt01/01-script.r
@@ -607,42 +607,40 @@ library(stringr)
 
 # Dividindo uma string em caracteres
 ?strsplit
-strsplit("Histogramas e Elementos de Dados", NULL)
+strsplit("Ciência de Dados com Linguagem R", NULL)
 
 # Dividindo uma string em caracteres, após o caracter espaço
-strsplit("Histogramas e Elementos de Dados", " ")
+strsplit("Ciência de Dados com Linguagem R", " ")
 
 # Trabalhando com strings
-string1 <- c("Esta é a primeira parte da minha string e será a primeira parte do meu vetor",
-             "Aqui a minha string continua, mas será transformada no segundo vetor")
-string1
+string_x <- c("Ciência de Dados", "Linguagem R")
+string_x
 
-string2 <- c("Precisamos testar outras strings - @???!§$",
-             "Análise de Dados em R")
-string2
+string_y <- c("Ciência de Dados", "Linguagem R")
+string_y
 
-# Adicionando 2 strings
-str_c(c(string1, string2), sep = "")
+# Adicionando duas strings
+str_c(c(string_x, string_y), sep = "")
 
-# Podemos contar quantas vezes um caracter aparece no texto
-str_count(string2, "s")
+# Contando quantas vezes um caracter aparece no texto
+str_count(string_y, "s")
 
-# Localiza a primeira e última posição em que o caracter aparece na string
-str_locate_all(string2, "s")
+# Localizando a primeira e última posição do caracter
+str_locate_all(string_y, "s")
 
 # Substitui a primeira ocorrência de um caracter
-str_replace(string2, "\\s", "")
+str_replace(string_y, "\\s", "")
 
 # Substitui todas as ocorrências de um caracter
-str_replace_all(string2, "\\s", "")
+str_replace_all(string_y, "\\s", "")
 
 # Detectando padrões nas strings
-string1 <- "17 jan 2001"
-string2 <- "1 jan 2001"
-padrao <- "jan 20"
-grepl(pattern = padrao, x = string1)
-padrao <- "jan20"
-grepl(pattern = padrao, x = string1)
+string_1 <- "Ciência de Dados com R"
+string_2 <- "Ciência de Dados - Linguagem R"
+padrao <- "com R"
+grepl(pattern = padrao, x = string_1)
+padrao <- "com r."
+grepl(pattern = padrao, x = string_1)
 
 # ========== # ========== # ========== # ========== # ========== #
 
